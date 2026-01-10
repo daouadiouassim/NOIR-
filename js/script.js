1165e52a6229be5bb12c6b7963cdc32075a84466
daouadiouassim <ouassimdaouadi@gmail.com>
2026-01-10 01:05:39 +0100

    Add menu toggle functionality for navigation

--- js/script.js
+++ js/script.js
@@ -27,3 +27,12 @@ document.querySelectorAll('a').forEach(link => {
     }
   });
 });
+const toggle = document.querySelector('.menu-toggle');
+const nav = document.querySelector('nav');
+
+if (toggle) {
+  toggle.addEventListener('click', () => {
+    nav.classList.toggle('active');
+  });
+}
+
